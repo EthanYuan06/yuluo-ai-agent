2cfa321dc4e2e0f54494623fd08e8334ac585fda
东山羽洛 <2329697573@qq.com>
2026-03-26 13:32:22 +0800

    初始化项目

---
+++ src/main/java/com/yuluo/yuluoaiagent/YuluoAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yuluo.yuluoaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuluoAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuluoAiAgentApplication.class, args);
+    }
+
+}
