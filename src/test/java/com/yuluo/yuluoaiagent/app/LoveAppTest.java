e4497c85bf3e0fc679406aef402f978ad64d8446
东山羽洛 <2329697573@qq.com>
2026-03-27 10:52:30 +0800

    多轮对话记忆

---
+++ src/test/java/com/yuluo/yuluoaiagent/app/LoveAppTest.java
@@ -0,0 +1,34 @@
+package com.yuluo.yuluoaiagent.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是牢大";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第二轮
+        message = "我喜欢安和昴，如何了解她？";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我喜欢的人叫什么来着，刚刚说过，请你帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+}
